512f3375362a379444b1f490b7778e5232c99f8e
VaroonSB <varoonsb1311@gmail.com>
2025-01-25 14:16:33 +0530

    feat: added hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import classes from "./styles/hero.module.css";
+
+export const Hero = () => {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src={"/images/site/My Photo.jpg"}
+          alt="An image showing Varoon"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I am Varoon</h1>
+      <p>I am a Full Stack Developer who loves Backend more than Frontend :)</p>
+    </section>
+  );
+};
